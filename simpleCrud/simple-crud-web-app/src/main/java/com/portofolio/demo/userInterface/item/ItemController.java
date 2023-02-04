f24b4c3f8a434b7711479534d7c4badad65d2821
DESKTOP-RIMA8UN\fabio <fabioguilherme1989@gmail.com>
2023-02-04 15:50:42 +0000

    refactor: the name of the modules changed and same package names

--- simpleCrud/simple-crud-web-app/src/main/java/com/portofolio/demo/userInterface/item/ItemController.java
+++ simpleCrud/simple-crud-web-app/src/main/java/com/portofolio/demo/userInterface/item/ItemController.java
@@ -1,10 +1,8 @@
 package com.portofolio.demo.userInterface.item;
 
 
-import com.portofolio.demo.domain.Item.ItemDomainService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemController {
 
 
-    private final ItemDomainService service;
-
+    //    private final ItemDomainService service;
+//
     private Logger log = LoggerFactory.getLogger(ItemController.class);
-
-    @Autowired
-    public ItemController(ItemDomainService service) {
-        this.service = service;
-    }
+//
+//    @Autowired
+//    public ItemController(ItemDomainService service) {
+//        this.service = service;
+//    }
 
     @PostMapping
     public void createItem() {
