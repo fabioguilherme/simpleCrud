719272dfe412894ef7224473d110fe266ee7bed0
DESKTOP-RIMA8UN\fabio <fabioguilherme1989@gmail.com>
2023-02-06 12:29:43 +0000

    cleaning the class IntegrationBaseTest

--- simpleCrud/simple-crud-core/src/test/java/com/portofolio/demo/IntegrationBaseTest.java
+++ simpleCrud/simple-crud-core/src/test/java/com/portofolio/demo/IntegrationBaseTest.java
@@ -3,12 +3,9 @@ package com.portofolio.demo;
 import com.portofolio.demo.config.JpaContextConfig;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {JpaContextConfig.class})
 public abstract class IntegrationBaseTest {
 
